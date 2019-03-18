2698ffc0a6693fcd2200da9bd01e5f68df2c0433
Dinesh Shrestha <dinesh.shrestha015@gmail.com>
2019-03-18 10:01:42 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+console.log("This is working");
+
+document.getElementById("menu").addEventListener("click", openMenu);
+
+function openMenu() {
+    document.getElementById("dropdown").classList.toggle("active");
+}
